44fe6a2cdd17b91ac7be84c29b5d663bf45cb183
D0486275 <d0486275@mail.fcu.edu.tw>
2019-08-17 13:10:27 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{data}}</h1>
+    <ve-histogram v-bind:data="chartData"></ve-histogram>
+    <ve-line :data="chartData"></ve-line>
+    <div class="btn" v-on:click="changeData">改變資料</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VeHistogram from 'v-charts/lib/histogram.common'
+import VeLine from 'v-charts/lib/line.common'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    VeHistogram,
+    VeLine
+  },
+  methods:{
+    changeData: function(){
+      this.chartData.rows[0].访问用户 += 100
+    }
+  },
+  data(){
+    return{
+      data:"Vue 有一點點難",
+      chartData: {
+          columns: ['日期', '访问用户', '下单用户', '下单率'],
+          rows: [
+            { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
+            { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
+            { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
+            { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
+            { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
+            { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
+          ]
+        }
+    }
+    
   }
 }
 </script>
@@ -24,5 +51,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn{
+  padding: 5px 10px;
 }
 </style>
